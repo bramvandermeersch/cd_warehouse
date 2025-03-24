f3b3035a024259d93daa8616c5c74b36080fddf6
Bram <bram.v@cervus.ai>
2025-03-24 15:59:50 +0000

    now baseline inventory management in there

--- src/warehouse.py
+++ src/warehouse.py
@@ -1,6 +1,26 @@
 class Warehouse:
     def __init__(self, cds):
-        pass
-
-    def buy_cd(self, title, cc_info):
+        self.cds = cds
+        
+    def buy_cd(self, album, cc_info):
+        if stock_info := self.check_stock(album):
+            if self.authorise_payment(stock_info['price'], cc_info):
+                self.remove_from_inventory(album)
+                return True
+            
+        return False # no CD sold
+            
+    def authorise_payment(self, price, cc_info):
+        # always succeeds for now...
         return True
+    
+    def check_stock(self, album):
+        if album := self.cds.get(album):
+            return album
+        
+    def remove_from_inventory(self, album):
+        if item := self.cds.get(album):
+            if item['stock'] > 1:
+                item['stock'] = item['stock'] - 1
+            else:
+                del self.cds[album]
--- tests/cd_warehouse_test.py
+++ tests/cd_warehouse_test.py
@@ -5,6 +5,16 @@ from src.warehouse import Warehouse
 
 
 class CdWarehouseTest(unittest.TestCase):
-    def test_by_cd(self):
-        warehouse = Warehouse([])
+    def test_buy_single_cd(self):
+        warehouse = Warehouse({"Foo Fighters":{"price":9.95, "stock":2}})
         self.assertEqual(True, warehouse.buy_cd('Foo Fighters', "12345"))
+
+    def test_buy_non_existing_cd(self):
+        warehouse = Warehouse({"Foo Fighters":{"price":9.95, "stock":2}})
+        self.assertEqual(False, warehouse.buy_cd('Oasis', "12345"))
+
+    def test_buy_too_many_cds(self):
+        warehouse = Warehouse({"Foo Fighters":{"price":9.95, "stock":2}})
+        self.assertEqual(True, warehouse.buy_cd('Foo Fighters', "12345"))
+        self.assertEqual(True, warehouse.buy_cd('Foo Fighters', "12345"))
+        self.assertEqual(False, warehouse.buy_cd('Foo Fighters', "12345"))
