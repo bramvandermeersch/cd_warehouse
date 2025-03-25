d9746aa2b2e9481c639f29513a8f4a60a60baad9
Bram <bram.v@cervus.ai>
2025-03-25 10:54:16 +0000

    extracted CD information into a separate CD class

--- src/warehouse.py
+++ src/warehouse.py
@@ -1,37 +1,46 @@
-
-
+class Cd:
+    def __init__(self, album, artist, title, price, stock):
+        self.album = album
+        self.artist= artist
+        self.title = title
+        self.price = price
+        self.stock = stock
 
 class Warehouse:
-    def __init__(self, cds, cc_processor):
-        self.cds = cds
+    def __init__(self, cd_store, cc_processor):
+        self.cd_store = cd_store
         self.cc_processor = cc_processor
         
     def buy_cd(self, album_name, cc_info):
-        if stock_info := self.check_stock(album_name):
-            if self.authorise_payment(stock_info['price'], cc_info):
+        if cd := self.check_stock(album_name):
+            if self.authorise_payment(cd.price, cc_info):
                 self.remove_from_inventory(album_name)
                 return True
             
-        return False # no CD sold
+        return False
             
     def authorise_payment(self, price, cc_info):
         # always succeeds for now...
         return self.cc_processor.authorise(price, cc_info)
     
     def check_stock(self, album_name):
-        if item := self.cds.get(album_name):
-            if item['stock'] > 0:
-                return item
-        
+        if cd := self.cd_store.get(album_name):
+            if cd.stock > 0:
+                return cd
+         
     def remove_from_inventory(self, album):
-        if item := self.cds.get(album):
-            if item['stock'] > 1:
-                item['stock'] = item['stock'] - 1
+        if item := self.cd_store.get(album):
+            if item.stock > 1:
+                item.stock = item.stock - 1
             else:
-                item['stock'] = 0
+                item.stock = 0
 
     def inventory(self):
-        return {album: item['stock'] for album, item in self.cds.items()}
+        return {album: item.stock for album, item in self.cd_store.items()}
 
     def find_albums(self, search_string):
-        return [album for album, item in self.cds.items() if search_string in album and item['stock'] > 0]
+        albums = []
+        for album_name, cd in self.cd_store.items():
+            if ((search_string in cd.artist) or (search_string in cd.title)) and (cd.stock > 0):
+                albums.append(album_name)
+        return albums
--- tests/cd_warehouse_test.py
+++ tests/cd_warehouse_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.warehouse import Warehouse
+from src.warehouse import Warehouse, Cd
 
 class CcMock():
     def __init__(self, result):
@@ -11,28 +11,28 @@ class CcMock():
 
 class CdWarehouseTest(unittest.TestCase):
     def test_buy_single_cd(self):
-        warehouse = Warehouse({"Foo Fighters":{"price":9.95, "stock":2}}, CcMock(True))
+        warehouse = Warehouse({"Foo Fighters":Cd("Foo Fighters", "Foo Fighters", "1998", 9.95, 2)}, CcMock(True))
         self.assertEqual(True, warehouse.buy_cd('Foo Fighters', "12345"))
 
     def test_buy_non_existing_cd(self):
-        warehouse = Warehouse({"Foo Fighters":{"price":9.95, "stock":2}}, CcMock(True))
+        warehouse = Warehouse({"Foo Fighters":Cd("Foo Fighters", "Foo Fighters", "1998", 9.95, 2)}, CcMock(True))
         self.assertEqual(False, warehouse.buy_cd('Oasis', "12345"))
 
     def test_buy_too_many_cds(self):
-        warehouse = Warehouse({"Foo Fighters":{"price":9.95, "stock":2}}, CcMock(True))
+        warehouse = Warehouse({"Foo Fighters":Cd("Foo Fighters", "Foo Fighters", "1998", 9.95, 2)}, CcMock(True))
         self.assertEqual(True, warehouse.buy_cd('Foo Fighters', "12345"))
         self.assertEqual(True, warehouse.buy_cd('Foo Fighters', "12345"))
         self.assertEqual(False, warehouse.buy_cd('Foo Fighters', "12345"))
 
     def test_failed_auth(self):
         cc_processor = CcMock(False)
-        warehouse = Warehouse({"Foo Fighters":{"price":9.95, "stock":2}}, cc_processor)
+        warehouse = Warehouse({"Foo Fighters":Cd("Foo Fighters", "Foo Fighters", "1998", 9.95, 2)}, cc_processor)
         self.assertEqual(False, warehouse.buy_cd('Foo Fighters', "12345"))
 
     def test_find_album(self):
         cc_processor = CcMock(False)
-        warehouse = Warehouse({"Foo Fighters":{"price":9.95, "stock":2},
-                               "Oasis":{"price":3.95, "stock": 10}}, cc_processor)
+        warehouse = Warehouse({"Foo Fighters":Cd("Foo Fighters", "Foo Fighters", "1998", 9.95, 2),
+                               "Oasis":Cd("Oasis", "Oasis", "1995", 3.95, 10)}, cc_processor)
         
         self.assertEqual([], warehouse.find_albums('Nirvana'))
         self.assertEqual(["Foo Fighters"], warehouse.find_albums('Foo Fighters'))
@@ -40,16 +40,16 @@ class CdWarehouseTest(unittest.TestCase):
         
     def test_get_warehouse_stock(self):
         cc_processor = CcMock(False)
-        warehouse = Warehouse({"Foo Fighters":{"price":9.95, "stock":2},
-                               "Oasis":{"price":3.95, "stock": 10}}, cc_processor)
+        warehouse = Warehouse({"Foo Fighters":Cd("Foo Fighters", "Foo Fighters", "1998", 9.95, 2),
+                               "Oasis":Cd("Oasis", "Oasis", "1995", 3.95, 10)}, cc_processor)
         
         self.assertEqual({"Foo Fighters": 2, "Oasis": 10}, warehouse.inventory())
                 
         
     def test_stock_update(self):
         cc_processor = CcMock(True)
-        warehouse = Warehouse({"Foo Fighters":{"price":9.95, "stock":2},
-                               "Oasis":{"price":3.95, "stock": 10}}, cc_processor)
+        warehouse = Warehouse({"Foo Fighters":Cd("Foo Fighters", "Foo Fighters", "1998", 9.95, 2),
+                               "Oasis":Cd("Oasis", "Oasis", "1995", 3.95, 10)}, cc_processor)
         
         warehouse.buy_cd("Foo Fighters", "12345")
         
@@ -58,8 +58,8 @@ class CdWarehouseTest(unittest.TestCase):
 
     def test_sold_out(self):
         cc_processor = CcMock(True)
-        warehouse = Warehouse({"Foo Fighters":{"price":9.95, "stock":2},
-                               "Oasis":{"price":3.95, "stock": 10}}, cc_processor)
+        warehouse = Warehouse({"Foo Fighters":Cd("Foo Fighters", "Foo Fighters", "1998", 9.95, 2),
+                               "Oasis":Cd("Oasis", "Oasis", "1995", 3.95, 10)}, cc_processor)
         
         warehouse.buy_cd("Foo Fighters", "12345")
         warehouse.buy_cd("Foo Fighters", "67890")
@@ -72,12 +72,12 @@ class CdWarehouseTest(unittest.TestCase):
 
         # we have 2x Foo Fighters in the warehouse
 
-        warehouse = Warehouse({"Foo Fighters":{"price":9.95, "stock":2},
-                               "Oasis":{"price":3.95, "stock": 10}}, cc_processor)
+        warehouse = Warehouse({"Foo Fighters":Cd("Foo Fighters", "Foo Fighters", "1998", 9.95, 2),
+                               "Oasis":Cd("Oasis", "Oasis", "1995", 3.95, 10)}, cc_processor)
         # we sell them both
         warehouse.buy_cd("Foo Fighters", "12345")
         warehouse.buy_cd("Foo Fighters", "67890")
 
         # if we list what's in stock foo fighters should not be in the list
         # of albums if we're searching for it.
-        self.assertEqual(False, "Foo Fighters" in warehouse.find_albums("Foo"))      
+        self.assertEqual(False, "Foo Fighters" in warehouse.find_albums("Foo"))
